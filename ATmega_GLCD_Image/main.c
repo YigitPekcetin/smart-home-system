bb013cf0d694efb24308a8e52936ec21c7e964f9
XenoverseUp <xenoverseup@gmail.com>
2024-01-18 16:04:23 +0300

    refactor:remove unnecessary declarations

--- ATmega_GLCD_Image/main.c
+++ ATmega_GLCD_Image/main.c
@@ -1,7 +1,3 @@
-/*
- * ATmega_GLCD_TextFont
- * http://electronicwings.com
- */
 #define F_CPU 8000000UL
 
 #include <avr/io.h> /* Include AVR std. library file */
@@ -29,7 +25,6 @@ void GLCD_Change(int x);
 void glcd_SetCursor(uint8_t x, uint8_t y);
 
 void adc_init();
-
 uint32_t readTouchX(void);
 uint32_t readTouchY(void);
 
@@ -45,11 +40,11 @@ uint8_t fullRotate(uint8_t value);
 uint64_t buffer[64];
 
 int main(void) {
-    glcd_Init(); /* Initialize GLCD */
     adc_init();
+    glcd_Init(); /* Initialize GLCD */
+
     _delay_ms(2);
     GLCD_Change(1);
-    clearBuffer();
 
     int velX = 1;
     int velY = 2;
@@ -193,12 +188,16 @@ uint8_t fullRotate(uint8_t value) {
 uint32_t readTouchX(void) {
     ADMUX = (1 << REFS0) | PA0;
     ADCSRA |= (1 << ADSC);
-    while (ADCSRA & (1 << ADSC))
+    while ((ADCSRA & (1 << ADIF)) == 0)
         ;
+    ADCSRA |= (1 << ADIF);
 
-    if (ADC < 50) return 65;
+    uint16_t reading = ADCL;
+    reading += ADCH << 8;
 
-    uint32_t val = (ADC * 142) / 1023;
+    if (reading < 50) return 65;
+
+    uint32_t val = (reading * 142) / 1023 + 1;
 
     return val;
 }
@@ -206,12 +205,16 @@ uint32_t readTouchX(void) {
 uint32_t readTouchY(void) {
     ADMUX = (1 << REFS0) | PA1;
     ADCSRA |= (1 << ADSC);
-    while (ADCSRA & (1 << ADSC))
+    while ((ADCSRA & (1 << ADIF)) == 0)
         ;
+    ADCSRA |= (1 << ADIF);
 
-    if (ADC < 50) return 65;
+    uint16_t reading = ADCL;
+    reading += ADCH << 8;
 
-    uint32_t val = (ADC * 142) / 1023;
+    if (reading < 50) return 10;
+
+    uint32_t val = (reading * 142) / 1023;
 
     return val;
 }
@@ -249,14 +252,12 @@ void GLCD_Data(char Data) /* GLCD data function */
 void GLCD_Change(int screen) {
     // false = left screen, true = right screen
 
-    PORTA = 0x0C;
     PORTD = 0x80;
     if (screen == 1)
         PORTB = 0x01;
     else
         PORTB = 0x02;
 
-    PORTD |= (1 << 7);
     GLCD_Command(0x3E);  /* Display OFF */
     GLCD_Command(0x42);  /* Set Y address (column=0) */
     GLCD_Command(0xB8);  /* Set x address (page=0) */
@@ -266,7 +267,7 @@ void GLCD_Change(int screen) {
 
 void glcd_Init() {
     // Set the direction of control and data pins
-    DDRA |= (1 << RS) | (1 << RW);
+    DDRA = (1 << RS) | (1 << RW);
     DDRD |= (1 << EN) | (1 << RST);
     DDRB |= (1 << CS1) | (1 << CS2);
     DDRC = 0xFF;  // Assuming the data port is on PORTC
@@ -274,7 +275,6 @@ void glcd_Init() {
     _delay_ms(20);
 
     // Initialize control pin states
-    PORTA = 0x00;  // Clear RS and RW
     PORTD = 0x00;  // Clear EN and RST
     PORTB = 0x00;  // Clear CS1 and CS2
 
@@ -290,7 +290,8 @@ void glcd_Init() {
 void adc_init() {
     ADMUX = (1 << REFS0);
     ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-    DDRA &= ~(1 << PA0) & ~(1 << PA1);
+    DDRA &= ~(1 << PA0);
+    DDRA &= ~(1 << PA1);
 }
 
 void glcd_SetCursor(uint8_t x, uint8_t y) {
